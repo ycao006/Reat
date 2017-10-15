5c1989b87d30de19b7d2d0c60738c13d77ba6036
yujie cao <yujiecao@yujies-macbook-pro.local>
2017-10-15 11:31:30 -0700

    Initial Commit

---
+++ Reat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Reat
+//
+//  Created by yujie cao on 10/15/17.
+//  Copyright © 2017 yujie cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Reat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Reat
+//
+//  Created by yujie cao on 10/15/17.
+//  Copyright © 2017 yujie cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
